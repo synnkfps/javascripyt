6bae05f1093847b784333b7648a4fe8750d1a089
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-09-09 18:12:46 -0300

    math.random() handling
    working javascript code :O

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import replacer 
 
 javascript = '''
-import random
+// input from the user
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
 // generating a random number
-const a = random.random() * (10-1) + 1
-console.log(`Random value between 1 and 10 is ${a}`);
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// display a random number
+console.log(`Random value between ${min} and ${max} is ${a}`);
 '''
 
 replacer.target = javascript
@@ -39,11 +44,9 @@ transpiled = ''''''
 
 for line in javascript.splitlines():
     if 'Math' in line:
-        if 'Math.random' in line:
-            pass
-        else:
-            transpiled += 'import math\n'
-            line = line.replace('Math', 'math')
+        transpiled += 'import math\nimport random\n\n'
+        line = line.replace('Math', 'math')
+        line = line.replace('math.random', 'random.random')
 
     if '`' in line and ('(' in line or ')' in line):
         line = line.replace('`', '\'')
@@ -72,4 +75,4 @@ for line in javascript.splitlines():
     transpiled += line 
 
 print(transpiled)
-#exec(transpiled)
+exec(transpiled)
